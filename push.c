947dc4ff9137f8b8afe310a411d7c941dac7e9ce
Ayaheddak <94086031+Ayaheddak@users.noreply.github.com>
2022-05-29 00:43:50 +0100

    Update push.c

--- push.c
+++ push.c
@@ -12,27 +12,35 @@
 
 #include "push_swap.h"
 
-void	push_a(t_list *a, t_list *b)
+void    push_a(t_list *a, t_list *b)
 {
-	t_node	*tmp;
-	t_node	*newnode;
-	int		value;
-
-	//value = get_lastelm (b);
-	newnode = addnode(value);
-	tmp = a->head;
-	while (tmp != NULL)
-	{
-		if (tmp->next == NULL)
-		{
-			tmp->next = newnode;
-			newnode->next = NULL;
-			break ;
-		}
-		tmp = tmp->next;
-	}
-	write(1, "pa\n", 3);
+  if(!empty_list(b))
+  {
+    add_elm_last(a,get_lastelm(b));
+    write(1, "pa\n", 3);
+  }
 }
+// void	push_a(t_list *a, t_list *b)
+// {
+// 	t_node	*tmp;
+// 	t_node	*newnode;
+// 	int		value;
+
+// 	//value = get_lastelm (b);
+// 	newnode = addnode(value);
+// 	tmp = a->head;
+// 	while (tmp != NULL)
+// 	{
+// 		if (tmp->next == NULL)
+// 		{
+// 			tmp->next = newnode;
+// 			newnode->next = NULL;
+// 			break ;
+// 		}
+// 		tmp = tmp->next;
+// 	}
+// 	write(1, "pa\n", 3);
+// }
 
 void	push_b(t_list *a, t_list *b)
 {
