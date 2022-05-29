fc1e2ffa5ce167575afcfad877f905395ba8f06a
Ayaheddak <94086031+Ayaheddak@users.noreply.github.com>
2022-05-29 04:43:52 +0100

    Update tessssst.c

--- tessssst.c
+++ tessssst.c
@@ -1,3 +1,25 @@
+// void three_elem(t_list *a)
+// {
+//   t_node *top;
+//   t_node *middle;
+//   t_node *bottom;
+  
+//   top = a->top;
+//   bottom  = a->head;
+//   midlle = a->head->next;
+  
+//   if(top->data > middle->data && middle->data < bottom->data && top->data > bottom->data)//case 1
+//     swap_a(a);
+//   else if (top->data < middle->data && middle->data > bottom->data && top->data > bottom->data)
+//   {
+//     swap_a(a);
+//     rev_rotate_a(a);
+//   }
+//   else if (top->data > middle->data && middle->data < bottom->data && top->data > bottom->data)
+//     rev_a(a);
+//   else if (top->data > middle->data && middle->data < bottom->data && top->data > bottom->data)
+//   else
+// }
 int get_lsize(t_list *l)
 {
   int len;
