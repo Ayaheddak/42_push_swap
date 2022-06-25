2f2d1d1e1bfd85cdb158d80551baa24e27917ffa
Ayaheddak <94086031+Ayaheddak@users.noreply.github.com>
2022-06-25 02:04:16 +0100

    Update checker.c

--- checker.c
+++ checker.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include "push_swap.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
